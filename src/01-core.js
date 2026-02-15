adcf60fc4ab97021d402da3f2294c6fa4db4d4a9
Kane <kmboswell@proton.me>
2026-02-15 09:54:19 -0600

    feat: Import the Commentations extension (#5)

--- src/01-core.js
+++ src/01-core.js
@@ -1,59 +1,82 @@
-/**
- * Core Extension Module
- * This is the main extension class that Scratch will register
- * Load this first (01-* naming convention)
- */
-
-// Import colorBlock from 02-example-module.js
-import { colorBlock } from './02-example-module.js';
-
-class TurboWarpExtension {
-  constructor() {
-    this.runtime = null;
-  }
-
-  /**
-   * Return extension info for Scratch
-   * This method is required by the Scratch extension protocol
-   */
+class Commentations {
   getInfo() {
     return {
-      id: 'myTurboWarpExtension',
-      name: 'My Extension',
-      color1: '#4CAF50',
-      color2: '#45a049',
-      color3: '#3d8b40',
-      menuIconURI: '',
-      blockIconURI: '',
+      id: 'kxCommentations',
+      name: 'Commentations',
+      color1: '#607D8B',
+      color2: '#455A64',
+      color3: '#455A64',
       blocks: [
         {
-          opcode: 'helloWorld',
-          blockType: 'reporter',
-          text: 'hello world',
-        },
-        {
-          opcode: 'add',
-          blockType: 'reporter',
-          text: '[A] + [B]',
+          opcode: 'label',
+          blockType: Scratch.BlockType.COMMAND,
+          text: 'new section: [TEXT]',
           arguments: {
-            A: {
-              type: 'number',
-              defaultValue: 0,
-            },
-            B: {
-              type: 'number',
-              defaultValue: 1,
+            TEXT: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'section label',
             },
           },
         },
         {
-          opcode: 'colorBlock',
-          blockType: 'reporter',
-          text: 'selected color [COLOR]',
+          opcode: 'note',
+          blockType: Scratch.BlockType.COMMAND,
+          text: 'note: [TEXT]',
           arguments: {
-            COLOR: {
-              type: 'color',
-              defaultValue: '#FF0000',
+            TEXT: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'an important note',
+            },
+          },
+        },
+        {
+          opcode: 'comment',
+          blockType: Scratch.BlockType.COMMAND,
+          text: 'comment: [TEXT]',
+          arguments: {
+            TEXT: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'comment',
+            },
+          },
+        },
+        {
+          opcode: 'todo',
+          blockType: Scratch.BlockType.COMMAND,
+          text: 'to-do: [TEXT]',
+          arguments: {
+            TEXT: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'fix this bug',
+            },
+          },
+        },
+        '---',
+        {
+          opcode: 'disableCode',
+          blockType: Scratch.BlockType.CONDITIONAL,
+          text: 'disable code with reason: [TEXT]',
+          branchCount: 1,
+          arguments: {
+            TEXT: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'reason for disabling',
+            },
+          },
+        },
+        '---',
+        {
+          opcode: 'explainValue',
+          blockType: Scratch.BlockType.REPORTER,
+          text: '[VALUE] comment: [TEXT]',
+          arguments: {
+            VALUE: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: '10',
+            },
+            TEXT: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'pixels per step',
             },
           },
         },
@@ -61,28 +84,37 @@ class TurboWarpExtension {
     };
   }
 
-  /**
-   * Block implementation: Hello World
-   */
-  helloWorld() {
-    return 'hello world!';
+  label(_args) {
+    // Does nothing, just a label
+    return;
   }
 
-  /**
-   * Block implementation: Add
-   */
-
-  add(args) {
-    return Number(args.A) + Number(args.B);
+  comment(_args) {
+    // Does nothing, just a comment
+    return;
   }
 
-  /**
-   * Block implementation: Color Block (delegates to 02-example-module.js)
-   */
-  colorBlock(args) {
-    return colorBlock(args);
+  note(_args) {
+    // I wish I could go lazy-bones mode and just use comment() for the note block,
+    // but I guess we can never have any good things in life
+    return;
+  }
+
+  todo(_args) {
+    // Does nothing, just a to-do marker
+    return;
+  }
+
+  disableCode(_args) {
+    // Returns false so the code inside the C-block never runs
+    return false;
+  }
+
+  explainValue(args) {
+    // Returns the value input, ignores the text input.
+    // This allows you to comment on specific numbers/strings.
+    return args.VALUE;
   }
 }
 
-// Register the extension
-Scratch.extensions.register(new TurboWarpExtension());
+Scratch.extensions.register(new Commentations());
